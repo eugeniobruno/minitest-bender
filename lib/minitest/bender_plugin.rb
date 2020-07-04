f95078869bee10ba9e55476ae90415902d234adb
Eugenio Bruno <eugeniobruno@gmail.com>
2020-07-04 14:06:58 -0300

    Do not implicitly enable the reporter when setting a configuration option

--- lib/minitest/bender_plugin.rb
+++ lib/minitest/bender_plugin.rb
@@ -8,48 +8,39 @@ module Minitest
 
     opts.on '--bender-mode=MODE', 'Bender: choose the mode of interaction with other reporters. (oblivious | cooperative)' do |m|
       Bender.configuration.mode = m
-      Bender.enable!
     end
 
     opts.on '--bender-recorder=RECORDER', 'Bender: choose how test results are printed as the suite runs. (compact | verbose | none)' do |r|
       Bender.configuration.recorder = r
-      Bender.enable!
     end
 
     opts.on('--bender-sections=SECTIONS', 'Bender: choose which sections to print. (comma-separated names)') do |ss|
       Bender.configuration.sections = ss
-      Bender.enable!
     end
 
     opts.on('--bender-sections-blacklist=SECTIONS', 'Bender: choose which sections to skip. (comma-separated names)') do |ss|
       Bender.configuration.sections_blacklist = ss
-      Bender.enable!
     end
 
     opts.on '--bender-overview-sort-key=KEY', 'Bender: choose how tests are sorted in the overview. (name | number)' do |k|
       Bender.configuration.overview_sort_key = k
-      Bender.enable!
     end
 
     opts.on '--bender-time-ranking-size=SIZE', 'Bender: choose the time ranking maximum size.' do |s|
       Bender.configuration.time_ranking_size = s
-      Bender.enable!
     end
 
     opts.on('--bender-backtrace-view=BV', 'Bender: choose the backtrace view for test errors. (user | full)') do |b|
       Bender.configuration.backtrace_view = b
-      Bender.enable!
     end
 
     opts.on('--bender-run-command=RC', 'Bender: set the prefix of rerun lines.') do |c|
       Bender.configuration.run_command = c
-      Bender.enable!
     end
 
     MinitestBender::Colorizer.color_keys.each do |color_key|
       opts.on("--bender-#{color_key}-color=COLOR", 'Bender: choose the different colors.') do |c|
         Bender.configuration.set_custom_color(color_key, c)
-        Bender.enable!
       end
     end
   end
