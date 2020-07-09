e471e062709210b09a885ca176fb57522ff91e46
Eugenio Bruno <eugeniobruno@gmail.com>
2020-07-09 18:20:34 -0300

    Change skip icon

--- lib/minitest-bender/states/skipped.rb
+++ lib/minitest-bender/states/skipped.rb
@@ -7,7 +7,7 @@ module MinitestBender
       COLOR = :skip
       LABEL = 'SKIPPED'
       GROUP_LABEL = 'SKIPS'
-      ICON = '/'
+      ICON = '○'
 
       def formatted_message(result)
         colored(result.failures[0].message)
@@ -17,7 +17,7 @@ module MinitestBender
         return '' if results.empty?
         skipped_count = results.size
         auxiliary_verb = skipped_count == 1 ? 'was' : 'were'
-        colored("#{results.size} #{auxiliary_verb} skipped")
+        colored("#{skipped_count} #{auxiliary_verb} skipped")
       end
     end
   end
