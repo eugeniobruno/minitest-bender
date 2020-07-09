6026ceea5a38f120d9a7a63ece17415a4ef16fc4
Eugenio Bruno <eugeniobruno@gmail.com>
2020-07-09 18:46:07 -0300

    Extract methods in time ranking section

--- lib/minitest-bender/sections/time_ranking.rb
+++ lib/minitest-bender/sections/time_ranking.rb
@@ -12,7 +12,7 @@ module MinitestBender
 
         io.puts(formatted_label)
         io.puts
-        results.sort_by { |r| -r.time }.take(size).each_with_index do |result, i|
+        sorted_results_to_show.each_with_index do |result, i|
           number = "#{i + 1})".ljust(4)
           io.puts "    #{number}#{result.formatted_time} #{result.formatted_name_with_context}"
         end
@@ -30,6 +30,14 @@ module MinitestBender
 
       def formatted_label
         "  #{Colorizer.colorize('TIME RANKING', :time, :bold, :underline)}"
+      end
+
+      def sorted_results_to_show
+        sorted_results.take(size)
+      end
+
+      def sorted_results
+        results.sort_by { |r| -r.time }
       end
 
       def print_divider
